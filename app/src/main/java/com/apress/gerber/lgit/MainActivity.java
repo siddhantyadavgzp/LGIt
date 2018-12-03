38fae56710a37ba7a790c663950de97aef350e0f
siddhantyadavgzp <42622546+siddhantyadavgzp@users.noreply.github.com>
2018-12-03 14:42:40 +0530

    Guess it is  Gsub change

--- app/src/main/java/com/apress/gerber/lgit/MainActivity.java
+++ app/src/main/java/com/apress/gerber/lgit/MainActivity.java
@@ -2,12 +2,47 @@ package com.apress.gerber.lgit;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button b1;
+    private EditText t1,t2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        onButtonClickEvent();
     }
+    public void onButtonClickEvent()
+    {
+        b1=(Button)findViewById(R.id.button);
+        t1=(EditText) findViewById(R.id.editText);
+        t2=(EditText)findViewById((R.id.editText2));
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Integer.parseInt(t2.getText().toString())>1)
+                {
+                    Toast.makeText(MainActivity.this,t1.getText().toString()+" is Not a faccha",Toast.LENGTH_LONG).show();
+                }
+                else
+                    Toast.makeText(MainActivity.this,t1.getText().toString()+" is a faccha",Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+
+
+
+    }
+
+
+
+
+
+
 }
